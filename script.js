b77c8e9a5f7fc7c1779f385b3659ee0e61dc1e06
Causalzap <causalzap@gmail.com>
2025-07-21 14:30:36 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".play-button").addEventListener("click", function() {
+    alert("Game will start now!");
+    // Here you could trigger the game or redirect to another page
+});
